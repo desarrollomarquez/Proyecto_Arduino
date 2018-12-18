7dd7d2138888cc0de03b7c9b0b1eb1463085f7de
Diego Márquez <desarrollo.marquez@gmail.com>
2018-12-18 05:54:11 -0500

    Ajuste a ip fija, revision de puerto 80

--- NodeMCU/Test_conexion/Test_WifiServer_ipfija/Test_WifiServer_ipfija.ino
+++ NodeMCU/Test_conexion/Test_WifiServer_ipfija/Test_WifiServer_ipfija.ino
@@ -3,131 +3,90 @@
 char* ssid = "Marquez Correa";
 char* password = "Marquez8355196";
 
-Arduino
+IPAddress staticIP(192,168,1,22);
+IPAddress gateway(192,168,1,9);
+IPAddress subnet(255,255,255,0);
 
-/*
- * Circuits4you.com
- * mDNS example ESP8266 in Arduino IDE
- * After connecting to WiFi router enter esp8266.local in web browser
-*/
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
+WiFiServer server(80);
 
-const char* wifiName = "circuits4you.com";
-const char* wifiPass = "your_password";
-  
-ESP8266WebServer server(80);
-
-//Handles http request 
-void handleRoot() {
-  digitalWrite(2, 0);   //Blinks on board led on page request 
-  server.send(200, "text/plain", "hello from esp8266!");
-  digitalWrite(2, 1);
-}
-
-
-// the setup function runs once when you press reset or power the board
-void setup() {
-  
+void setup(void)
+{
   Serial.begin(115200);
-  delay(10);
-  // We start by connecting to a WiFi network
   Serial.println();
-  
-  Serial.print("Connecting to ");
-  Serial.println(wifiName);
 
-  WiFi.begin(wifiName, wifiPass);
-
-  while (WiFi.status() != WL_CONNECTED) {
+  Serial.printf("Conectando a %s\n", ssid);
+  WiFi.begin(ssid, password);
+  WiFi.config(staticIP, gateway, subnet);
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());   //You can get IP address assigned to ESP
-
-   if(WiFi.status() == WL_CONNECTED) //If WiFi connected to hot spot then start mDNS
-  {
-    if (MDNS.begin("esp8266")) {  //Start mDNS with name esp8266
-      Serial.println("MDNS started");
-    }
-  }
-
-  server.on("/", handleRoot);  //Associate handler function to path
-    
-  server.begin();                           //Start server
-  Serial.println("HTTP server started");
-}
-
-// the loop function runs over and over again forever
-void loop() {
-  server.handleClient();
-}
-
-
-/*
- * Circuits4you.com
- * mDNS example ESP8266 in Arduino IDE
- * After connecting to WiFi router enter esp8266.local in web browser
-*/
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
- 
-const char* wifiName = "circuits4you.com";
-const char* wifiPass = "your_password";
   
-ESP8266WebServer server(80);
- 
-//Handles http request 
-void handleRoot() {
-  digitalWrite(2, 0);   //Blinks on board led on page request 
-  server.send(200, "text/plain", "hello from esp8266!");
-  digitalWrite(2, 1);
-}
- 
- 
-// the setup function runs once when you press reset or power the board
-void setup() {
+    // Start the server
+  server.begin();
+  Serial.println("Server started");
   
-  Serial.begin(115200);
-  delay(10);
-  // We start by connecting to a WiFi network
+  // prepare GPIO14
+  pinMode(14, OUTPUT);
+  digitalWrite(14, 0);
+
+
+  
   Serial.println();
-  
-  Serial.print("Connecting to ");
-  Serial.println(wifiName);
- 
-  WiFi.begin(wifiName, wifiPass);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
- 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());   //You can get IP address assigned to ESP
- 
-   if(WiFi.status() == WL_CONNECTED) //If WiFi connected to hot spot then start mDNS
-  {
-    if (MDNS.begin("esp8266")) {  //Start mDNS with name esp8266
-      Serial.println("MDNS started");
-    }
-  }
- 
-  server.on("/", handleRoot);  //Associate handler function to path
-    
-  server.begin();                           //Start server
-  Serial.println("HTTP server started");
+  Serial.print("Conectado, dirección IP: ");
+  Serial.println(WiFi.localIP());
 }
- 
-// the loop function runs over and over again forever
+
 void loop() {
-  server.handleClient();
-}
+  
+   // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+  
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+  
+  // Match the request
+  int val;
+  if (req.indexOf("/rele/0") != -1)
+    val = 0;
+  else if (req.indexOf("/rele/1") != -1)
+    val = 1;
+  else {
+    Serial.println("invalid request");
+    client.stop();
+    return;
+  }
+
+  // Set GPIO2 according to the request
+  digitalWrite(14, val);
+  
+  client.flush();
+
+  // Prepare the response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n\r\n<html>\r\nEl rele esta ";
+  s += (val)?"activado":"desactivado";
+  s += "</html>\n";
+
+  // Send the response to the client
+  client.print(s);
+  delay(1);
+  Serial.println("Client disonnected");
+
+  // The client will actually be disconnected 
+  // when the function returns and 'client' object is detroyed
+  
+  
+  
+  }
