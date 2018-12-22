4aa77315ca3b3ab4b72fcac5a486bc36e9c74aa8
Diego Márquez <desarrollo.marquez@gmail.com>
2018-12-21 22:17:05 -0500

    Adicion modulo de request

--- NodeMCU/Test_conexion/Test_LCD2x16/Test_LCD2x16.ino
+++ NodeMCU/Test_conexion/Test_LCD2x16/Test_LCD2x16.ino
@@ -20,4 +20,52 @@ lcd.print("Hello, NodeMCU");
 
 }
 
-void loop() { // do nothing here }
+void loop() { // do nothing here
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+  
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+  
+  // Match the request
+  int val;
+  if (req.indexOf("/rele/0") != -1)
+    val = 0;
+  else if (req.indexOf("/rele/1") != -1)
+    val = 1;
+  else {
+    Serial.println("invalid request");
+    client.stop();
+    return;
+  }
+
+  // Set GPIO2 according to the request
+  digitalWrite(14, val);
+  
+  client.flush();
+
+  // Prepare the response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n\r\n<html>\r\nEl rele esta ";
+  s += (val)?"activado":"desactivado";
+  s += "</html>\n";
+
+  // Send the response to the client
+  client.print(s);
+  delay(1);
+  Serial.println("Client disonnected");
+
+  // The client will actually be disconnected 
+  // when the function returns and 'client' object is detroyed
+  
+  }
