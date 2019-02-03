d641ec2f19e1353a692f9bdf0298bc17fe5cfd07
Diego Márquez <desarrollo.marquez@gmail.com>
2019-02-03 16:10:43 -0500

    Ajuste en  proyecto motor paso a paso en grados

--- NodeMCU/Test_conexion/Test_WifiServer_MotorStep/Test_WifiServer_MotorStep.ino
+++ NodeMCU/Test_conexion/Test_WifiServer_MotorStep/Test_WifiServer_MotorStep.ino
@@ -3,20 +3,28 @@
 #include <Stepper.h>
 
 // Esto es el número de pasos en un minuto
-#define STEPS 4096 
+#define STEPS 200 
 // Número de pasos que queremos que de
 #define NUMSTEPS 20
 
 // Constructor, pasamos STEPS y los pines donde tengamos conectado el motor
 Stepper stepper(STEPS, 2, 14, 12, 13);
 
-void setup() {
-  // Asignamos la velocidad en RPM (Revoluciones por Minuto)
-  stepper.setSpeed(5);
+void setup()
+{
+  // Velocidad del motor en RPM
+  stepper.setSpeed(100);
 }
-
-void loop() {
-  // Movemos el motor un número determinado de pasos
-  stepper.step(NUMSTEPS);
-  delay(200);
+ 
+void loop()
+{
+  //Girar una vuelta entera en un sentido
+  //stepper.step(200);
+  //delay(500); //Pequeña pausa
+  for (int x=0; x< 8; x++) {
+    stepper.step(STEPS);
+    delay(500);
+  }
+  
+ 
 }
