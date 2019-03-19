18971c050e4754d574997173ee68cacdb2df42fc
Diego Márquez <desarrollo.marquez@gmail.com>
2019-03-18 21:56:59 -0500

    Ajuste proyecto PID basico para variables de entrada

--- NodeMCU/Test_conexion/Test_WifiServer_PID_basico/Test_WifiServer_PID_basico.ino
+++ NodeMCU/Test_conexion/Test_WifiServer_PID_basico/Test_WifiServer_PID_basico.ino
@@ -1,53 +1,236 @@
-/********************************************************
- * PID Adaptive Tuning Example
- * One of the benefits of the PID library is that you can
- * change the tuning parameters at any time.  this can be
- * helpful if we want the controller to be agressive at some
- * times, and conservative at others.   in the example below
- * we set the controller to use Conservative Tuning Parameters
- * when we're near setpoint and more agressive Tuning
- * Parameters when we're farther away.
- ********************************************************/
 
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <Wire.h>  // This library is already built in to the Arduino IDE
+#include <LiquidCrystal_I2C.h> //This library you can add via Include Library > Manage Library >
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include <PID_v1.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const char* host = "icaro";
+const char* ssid     = "Marquez Correa";
+const char* password = "Marquez8355196";
+
+//const char* ssid     = "pegajoso";
+//const char* password = "pegajososiempre";
+
+ESP8266WebServer server(80);
+
+
+float temp_hot, temp_cold;  // Values read from sensor
+int val = 1;
+int tiempo_apertura = 3000;
+int start_pwm;
+int valor_pwm;
+int salidaPWM = 15;  // salida de señal PWM
 
 //Define Variables we'll be connecting to
 double Setpoint, Input, Output;
 
-//Define the aggressive and conservative Tuning Parameters
-double aggKp=4, aggKi=0.2, aggKd=1;
-double consKp=1, consKi=0.05, consKd=0.25;
+//Define the aggressive and conservative Tuning Parameters PID
+double aggKp=1, aggKi=0.2, aggKd=1;
+double consKp=4, consKi=0.05, consKd=0.25;
 
+//Mensajes del API y LCD
+int PWM_duty = 0;
+String webString="";
+String lcdString="";    
+unsigned long previousMillis = 0;        // will store last temp was read
+const long interval = 2000;              // interval at which to read sensor
+
+// Pin donde se conecta el bus 1-Wire
+const int pinDatosDQHot  = 0;
+const int pinDatosDQCold = 2;
+bool s=false;
+
+// Instancia a las clases OneWire y DallasTemperature
+OneWire oneWireObjetoHot(pinDatosDQHot);
+OneWire oneWireObjetoCold(pinDatosDQCold);
+DallasTemperature sensorDS18B20Hot(&oneWireObjetoHot);
+DallasTemperature sensorDS18B20Cold(&oneWireObjetoCold);
 //Specify the links and initial tuning parameters
 PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
 
-void setup()
-{
-  //initialize the variables we're linked to
-  Input = analogRead(0);
-  Setpoint = 100;
 
-  //turn the PID on
+void handle_root() {
+  webString="Bienvenido API REST CSV  server: Metodos  /open -> Abrir Compuerta; /temperaturas -> Mostrar Temperaturas Hot y Cold; /cook/1 -> Cocinar - /cook/0 -> No cocinar";
+  pushMsg(webString);
+  delay(100);
+}
+ 
+void setup(void)
+{
+  lcd.begin();   // initializing the LCD
+  lcd.backlight();
+    
+  // prepare GPIO16 - PWM
+  pinMode(16, OUTPUT);
+  pinMode(salidaPWM, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  Setpoint = 100; // velocidad referencia
+  
+  sensorDS18B20Hot.begin(); // initialize temperature sensor
+  sensorDS18B20Cold.begin(); 
+
+   //Activar el PID
+  myPID.SetSampleTime(20);
   myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(0,1024);
+  
+  Serial.begin(115200);
+  
+  // Connect to WiFi networkRegístrese hoy mismo 
+  Serial.print("\n\r \n\rWorking to connect");
+  WiFi.begin(ssid, password);
+  // Wait for connection
+
+  // Conectado; imprimimos la IP local por Serial.
+  Serial.println(WiFi.localIP());
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+
+  
+  //Serial.println("");
+  Serial.println("Reading Server");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  
+  
+  //Rest API
+  MDNS.begin(host);
+  MDNS.addService("http", "tcp", 80);
+  Serial.printf("Ready! Open http://%s.local in your browser\n", host);
+  server.on("/", handle_root);
+  
+  server.on("/temperaturas", [](){  
+    gettemperatura();       // read sensor
+    String json="Temperatura_Hot: "+String((int)temp_hot)+" Grados"+"Temperatura_Cold: "+String((int)temp_cold)+" Grados";
+    pushMsg(json);
+  });
+
+ 
+  server.on("/open", [](){  
+    val = 0;
+    String json="Compuerta Open: "+String((int)val);
+    pushMsg(json);
+  });
+
+  server.on("/cook/1", [](){  
+    start_pwm = 1;
+    String json="Cocinar: "+String((int)start_pwm);
+    pushMsg(json);
+  });
+
+  
+  server.on("/cook/0", [](){  
+    start_pwm = 0;
+    String json="No Cocinar: "+String((int)start_pwm);
+    pushMsg(json);
+    
+  });
+  
+  server.on("/api", [](){  
+    gettemperatura(); // read sensor
+    String json="{\"temperatura_hot\":"+String((int)temp_hot)+"}, {\"temperatura_cold\":"+String((int)temp_cold)+"}";
+    Serial.println(json);
+    pushMsg(json);
+  });
+  
+  server.begin();
+  Serial.println("HTTP server started");
+  
+  
+}
+ 
+void loop(void)
+{  
+  server.handleClient();
+  gettemperatura();
+  pushPWM();
+  
+ 
+} 
+
+
+void pushLCD(String msg){
+      String hs="IP:"+String( WiFi.localIP().toString().c_str());
+      lcd.setCursor(0, 0);
+      lcd.print(msg);
+      lcd.setCursor(0, 1);
+      lcd.print(hs);  
 }
 
-void loop()
-{
-  Input = analogRead(0);
-  
+void gettemperatura() {
+     unsigned long currentMillis = millis();
+     if(currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        sensorDS18B20Hot.requestTemperatures();
+        temp_hot =  sensorDS18B20Hot.getTempCByIndex(0);
+        sensorDS18B20Cold.requestTemperatures();
+        temp_cold =  sensorDS18B20Cold.getTempCByIndex(0);
+      }
+      //lcdString ="Hot:"+String((int)temp_hot)+"C"+" Cold:"+String((int)temp_cold)+"C";
+      //pushLCD(lcdString);
+}
+
+void pushOpen(){  
+      if(val == 0){
+        digitalWrite(13, val);    
+        delay(tiempo_apertura);
+        val = 1;
+        pushLCD(" Compuerta Open ");
+
+        
+      }
+      else{
+       digitalWrite(13, val); 
+       webString="Compuerta Close: "+String((int)val);
+       pushMsg(webString);
+      }
+}
+
+void pushMsg(String json){
+    server.send(200, "text/plain", json);
+}
+
+void pushPWM() {
+
+  Setpoint = 60; // velocidad referencia
+  Input = temp_hot;
   double gap = abs(Setpoint-Input); //distance away from setpoint
-  if(gap<10)
+  
+  if(gap < 35 )
   {  //we're close to setpoint, use conservative tuning parameters
-    myPID.SetTunings(consKp, consKi, consKd);
+       myPID.SetTunings(aggKp, aggKi, aggKd);
   }
   else
   {
      //we're far from setpoint, use aggressive tuning parameters
-     myPID.SetTunings(aggKp, aggKi, aggKd);
+     myPID.SetTunings(consKp, consKi, consKd);
   }
   
   myPID.Compute();
-  analogWrite(3,Output);
+  analogWrite(salidaPWM,Output);
+  //pushLCD(String((int)Output));
+  delay(1000);
+  lcdString ="Hot:"+String((int)temp_hot)+" "+"PWM:"+String((int)Output);
+  pushLCD(lcdString);
+
+      
 }
+
 
 
