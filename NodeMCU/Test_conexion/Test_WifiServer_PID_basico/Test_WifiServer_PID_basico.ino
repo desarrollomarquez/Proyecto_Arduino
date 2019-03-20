5cbc5ddc9ded81b7ebf09fb773b20728e331d618
Diego Márquez <desarrollo.marquez@gmail.com>
2019-03-19 22:58:49 -0500

    Ajuste proyecto PID basico para variables de entrada Kp, Ki

--- NodeMCU/Test_conexion/Test_WifiServer_PID_basico/Test_WifiServer_PID_basico.ino
+++ NodeMCU/Test_conexion/Test_WifiServer_PID_basico/Test_WifiServer_PID_basico.ino
@@ -33,8 +33,8 @@ int salidaPWM = 15;  // salida de señal PWM
 double Setpoint, Input, Output;
 
 //Define the aggressive and conservative Tuning Parameters PID
-double aggKp=4, aggKi=0.2, aggKd=1;
-//double consKp=1, consKi=0.05, consKd=0.25;
+double aggKp=1, aggKi=2.5, aggKd=2;
+//double consKp=-1, consKi=-0.05, consKd=-0.25;
 double consKp=2, consKi=5, consKd=1;
 //Mensajes del API y LCD
 int PWM_duty = 0;
@@ -208,11 +208,11 @@ void pushMsg(String json){
 
 void pushPWM() {
 
-  Setpoint = 70; // velocidad referencia
-  Input = temp_hot;
-  double gap = abs(Setpoint-Input); //distance away from setpoint
+  //Setpoint = 35; // velocidad referencia
+  //Input = temp_hot;
+  //double gap = abs(Setpoint-Input); //distance away from setpoint
   
-  if(gap < 35 )
+  if(temp_hot < 35 )
   {  //we're close to setpoint, use conservative tuning parameters
       myPID.SetTunings(consKp, consKi, consKd);
   }
